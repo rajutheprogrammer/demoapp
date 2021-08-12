b141d6fdf41fbf6eafd91366363beb12e195933a
rajutheprogrammer <onlyrchandra@gmail.com>
2021-08-12 13:56:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// start here
+// starts all functionality
+var main = function () {
+  // gets canvas element in canvas variable
+  var canvas = document.getElementById("mycanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.moveTo(0, 0);
+  ctx.lineTo(600, 500);
+  ctx.stroke();
+}
+
+ 
+ 
+
+
+ 
+  
+  
